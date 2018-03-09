aada37bebd47978c8fa5a8ba51f0198c804e4c3f
Dan Hoffman <dghoffman@gmail.com>
2018-03-08 20:50:06 -0500

    fixed bug in steps.js (transition wasn't working). improved form validation.

--- src/ClassroomManager/ClassroomManager/wwwroot/js/site.js
+++ src/ClassroomManager/ClassroomManager/wwwroot/js/site.js
@@ -3,15 +3,10 @@
 form.steps({
     headerTag: "h3",
     bodyTag: "fieldset",
-    transitionEffect: "slideLeft",
     onStepChanging: function (event, currentIndex, newIndex) {
         // Allways allow previous action even if the current form is not valid!
         if (currentIndex > newIndex) {
             return true;
-        }
-        // Forbid next action on "Warning" step if the user is to young
-        if (newIndex === 3 && Number($("#age-2").val()) < 18) {
-            return false;
         }
         // Needed in some cases if the user went back (clean up)
         if (currentIndex < newIndex) {
@@ -23,10 +18,6 @@ form.steps({
         return form.valid();
     },
     onStepChanged: function (event, currentIndex, priorIndex) {
-        // Used to skip the "Warning" step if the user is old enough.
-        if (currentIndex === 2 && Number($("#age-2").val()) >= 18) {
-            form.steps("next");
-        }
         // Used to skip the "Warning" step if the user is old enough and wants to the previous step.
         if (currentIndex === 2 && priorIndex === 3) {
             form.steps("previous");
